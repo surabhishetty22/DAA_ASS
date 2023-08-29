fe45571194a61919d9931b682a2ffefbbbc84a1d
surabhishetty22 <116055119+surabhishetty22@users.noreply.github.com>
2023-08-29 18:08:03 +0000

    commit

---
+++ obst.py
@@ -0,0 +1,33 @@
+def optimal_bst(keys, freq):
+    n = len(keys)
+    dp = [[0] * (n + 1) for _ in range(n + 1)]
+
+    for i in range(n):
+        dp[i][i + 1] = freq[i]
+
+    for length in range(2, n + 1):
+        for i in range(n - length + 2):
+            j = i + length - 1
+            dp[i][j] = float('inf')
+            sum_freq = sum(freq[i:j+1])
+
+            for k in range(i, j + 1):
+                temp = dp[i][k] + dp[k + 1][j] + sum_freq
+                if temp < dp[i][j]:
+                    dp[i][j] = temp
+
+    return dp[0][n]
+
+if __name__ == "__main__":
+    keys = input("Enter keys separated by spaces: ").split()
+    freq = list(map(int, input("Enter corresponding frequencies: ").split()))
+
+    result = optimal_bst(keys, freq)
+    print("Optimal binary search tree cost:", result)
+
+
+
+# output:
+# Enter keys separated by spaces: A B C
+# Enter corresponding frequencies: 2 4 6
+# Optimal binary search tree cost: 34
